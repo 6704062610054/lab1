601665781f37bdbe2f1267e1f669b2a3b0299362
6704062610054 <s6704062610054@email.kmutnb.ac.th>
2024-11-26 16:16:41 +0700

    ข้อที่ 1

---
+++ lab1minx.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a = 1;
+	int b = 2;
+	if (a>b){
+		printf("a is more than b");
+	}
+	else if (b>a){
+		printf("b is more than a");
+	}
+	else if (a==b){
+		printf("a eqauls b");
+	}
+}
